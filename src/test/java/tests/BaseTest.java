7acae2ae6ecf943e59ecc884930887cc92cf6f95
danya0865 <danon.pankratov03@mail.ru>
2025-03-29 19:14:15 +0300

    Выполненное первое задание

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,4 +1,31 @@
 package tests;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import java.time.Duration;
+
 public class BaseTest {
+    protected WebDriver driver;
+
+    @BeforeClass
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    protected void openUrl(String url) {
+        driver.get(url);
+    }
 }
--- src/test/java/tests/SortCustomersTest.java
+++ src/test/java/tests/SortCustomersTest.java
@@ -1,4 +1,35 @@
 package tests;
 
-public class SortCustomersTest {
+import io.qameta.allure.*;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pages.CustomerPage;
+
+import java.util.List;
+
+public class SortCustomersTest extends BaseTest {
+
+    @Test
+    @Epic("Banking Application")
+    @Feature("Customer Management")
+    @Story("Sort Customers")
+    @Description("Verify that customers can be sorted by First Name.")
+    public void testSortCustomersByFirstName() {
+        String startUrl = "https://www.globalsqa.com/angularJs-protractor/BankingProject/#/manager/list";
+        openUrl(startUrl);
+
+        CustomerPage customerPage = new CustomerPage(driver);
+
+        List<String> initialNames = customerPage.getCustomerFirstNames();
+
+        customerPage.sortCustomersByFirstName();
+
+        List<String> sortedNames = customerPage.getCustomerFirstNames();
+
+        List<String> expectedSortedNames = initialNames.stream()
+                .sorted()
+                .toList();
+
+        Assert.assertEquals(sortedNames, expectedSortedNames, "Customers are not sorted correctly.");
+    }
 }
